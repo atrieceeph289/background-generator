f4ef72ccdce434af794e3c64972b880ae4c654e7
Alex Triece <alex.triece@gmail.com>
2020-09-18 17:17:40 -0500

    adding starting project

---
+++ Bgen.js
@@ -0,0 +1,22 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1")
+var color2 = document.querySelector(".color2")
+var body = document.getElementById("gradient")
+var rand = document.querySelector(".random")
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")"
+    rand.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")"
+    css.textContent = body.style.background + ";"
+}
+
+function randBackground() {
+    color1.value = '#'+Math.random().toString(16).substr(2,6);
+    color2.value = '#'+Math.random().toString(16).substr(2,6);
+    setGradient();
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+rand.addEventListener("click", randBackground);
+window.onload = setGradient(), randBackground();
+
